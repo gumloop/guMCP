9b513cd26f59c94c7f999beb7da3532e25903c67
Sanskar Khandelwal <sanskar.khandelwal@cognida.ai>
2025-05-06 00:21:00 +0530

    Update regex extraction logic in run_tool_test to ignore the first two lines of the response for improved accuracy

--- tests/utils/test_tools.py
+++ tests/utils/test_tools.py
@@ -99,8 +99,8 @@ async def run_tool_test(client, context: dict, test_config: dict) -> dict:
     if "regex_extractors" in test_config:
         for key, pattern in test_config["regex_extractors"].items():
             match = re.search(
-                pattern, response.split("\n")[-1], re.DOTALL | re.IGNORECASE
-            )  # split("\n")[-1] to get the last line of the response to avoid the calling tool message (line 124 - /tests/clients/LocalMCPTestClient.py)
+                pattern, " ".join(response.split("\n")[2:]), re.DOTALL | re.IGNORECASE
+            )  # split("\n")[2:] to ignore first  line of the response to avoid the calling tool message (line 124 - /tests/clients/LocalMCPTestClient.py)
             if match and len(match.groups()) > 0:
                 context[key] = match.group(1).strip()
 
